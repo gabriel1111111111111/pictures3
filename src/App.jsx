6d9237d2242db49469aaa321259ecd485b325a63
gabriel1111111111111 <gabdua10@gmail.com>
2024-04-22 14:49:09 -0700

    add css styles to the project

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,20 @@ function App() {
 
   const handleSubmit = async(term) => {
     console.log('uste esta buscando con:', term)
+    const result = await searchImage(term)
+
+    console.log('coco')
+    console.log(result)
+
+    setImages(result)
+
   }
 
   return (
       <div>
        <h1>App</h1>
        <SearchBar onSubmit={handleSubmit} />
+       <ImageList image={images} />
        </div>
   )
 }
